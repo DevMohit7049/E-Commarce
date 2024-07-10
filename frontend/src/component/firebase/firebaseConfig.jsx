e17e028bc6fcdcb4efa3e3f829cce57f457280f2
Mohit tomar <tomarmohit030@gmail.com>
2024-07-10 11:35:30 +0530

    major chnages

--- frontend/src/component/firebase/firebaseConfig.jsx
+++ frontend/src/component/firebase/firebaseConfig.jsx
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from '@firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDXyQ-neKB06W5ecgRG4OcDLY3-pCJ0ykc",
-  authDomain: "e-commarceauth.firebaseapp.com",
-  projectId: "e-commarceauth",
-  storageBucket: "e-commarceauth.appspot.com",
-  messagingSenderId: "890764968902",
-  appId: "1:890764968902:web:d6a362062b88dd80b428c7"
+  apiKey: API_KEY ,
+  authDomain: REACT_APP_AUTHDOMAIN,
+  projectId: PROJECTID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID
 };
 
 // Initialize Firebase
